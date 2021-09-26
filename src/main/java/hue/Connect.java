77b068f61da59b29206c7a0770095117d99ac085
Matti <mateusz.surma.f@gmail.com>
2021-09-26 19:39:30 +0200

    Revert "autoformat"
    This reverts commit 7277ffeeb50008f8b4127540a0b0bb019629bdf1.

--- src/main/java/hue/Connect.java
+++ src/main/java/hue/Connect.java
@@ -8,7 +8,7 @@ public class Connect {
 
     public static final String BRIDGE_ADDRESS = "http://" + readXMLFile.getBridgeAddress();
     public static final String API = "/api";
-    public static final String USERNAME = "/" + readXMLFile.getUsername();
+    public static final String USERNAME = "/"+ readXMLFile.getUsername();
 
     public static final String LIGHTS = "/lights";
     public static final String DEN_LIGHTS_ID = "/2";
