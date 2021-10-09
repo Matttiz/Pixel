fca8af49b5f41315e98a47fad93760c9a2c5f3f6
Matti <mateusz.surma.f@gmail.com>
2021-10-09 16:35:33 +0200

    add select part of screen

--- src/main/java/averagePixel/DisplayColor.java
+++ src/main/java/averagePixel/DisplayColor.java
@@ -6,14 +6,22 @@ import java.awt.*;
 
 public class DisplayColor {
 
+    private static int quarter;
+    private static int  witchQuarter;
+
     private final int height = Toolkit.getDefaultToolkit().getScreenSize().height;
     private final int width = Toolkit.getDefaultToolkit().getScreenSize().width;
     private final int sampleHeight = 600;
     private final int sampleWidth = 600;
 
-    private final int pixelsNumber = (height * width) / (sampleHeight * sampleWidth);
-    private final int pixelsHeightToCheck = height / sampleHeight;
-    private final int pixelsWidthToCheck = width / sampleWidth;
+    private final int pixelsNumber = (height * width / quarter) / (sampleHeight * sampleWidth);
+    private final int pixelsHeightToCheck = (int) ((height / sampleHeight) / Math.sqrt(quarter));
+    private final int pixelsWidthToCheck = (int) ((width / sampleWidth) / Math.sqrt(quarter));
+
+    private static int pixelHeightStart;
+    private static int pixelWidthStart;
+    private static SelectedPart part = new SelectedPart();
+
 
     private Color color;
     private int blue;
@@ -21,10 +29,18 @@ public class DisplayColor {
     private int red;
 
     @SneakyThrows
-    public DisplayColor() {
+    public DisplayColor(int quarter, int witchQuarter) {
+        if (!part.isSet()){
+            part = new SelectedPart(quarter,witchQuarter);
+            pixelHeightStart = part.getStartHeightPart() * pixelsHeightToCheck ;
+            pixelWidthStart = part.getStartWidthPart() * pixelsWidthToCheck;
+
+        }
+
+
         PixelColor pixel = new PixelColor();
-        for (int i = 0; i < pixelsWidthToCheck; i++) {
-            for (int j = 0; j < pixelsHeightToCheck; j++) {
+        for (int i = pixelWidthStart; i <pixelWidthStart +  pixelsWidthToCheck; i++) {
+            for (int j = pixelHeightStart; j < pixelHeightStart + pixelsHeightToCheck; j++) {
                 pixel.getColor(i * sampleHeight, j * sampleWidth);
                 blue += pixel.getBlue();
                 red += pixel.getRed();
@@ -42,5 +58,14 @@ public class DisplayColor {
 
     public Color getPixelColor() {
         return this.color;
+    }
+
+    public void setHeightQuarter(int witchQuarter){
+        int  sqrt  = (int) (Math.sqrt(quarter));
+        int all = (int) witchQuarter/sqrt;
+        int part = (int) witchQuarter%sqrt;
+
+        int startHeight = all + pixelsHeightToCheck/sqrt;
+        int startWidth = part + pixelsWidthToCheck/sqrt;
     }
 }
---
+++ src/main/java/averagePixel/SelectedPart.java
@@ -0,0 +1,35 @@
+package averagePixel;
+
+public class SelectedPart {
+
+
+    private int quarter;
+    private int  witchQuarter;
+    private int startHeightPart;
+    private int startWidthPart;
+    private boolean set = false;
+
+    public SelectedPart(){
+    }
+
+    public SelectedPart(int quarter, int witchQuarter) {
+        this.quarter = quarter;
+        int sqrt = (int) (Math.sqrt(quarter));
+        this.witchQuarter = witchQuarter;
+        this.startHeightPart = (int) witchQuarter / sqrt;
+        this.startWidthPart = (int) witchQuarter % sqrt;
+        set = true;
+    }
+
+    public boolean isSet() {
+        return set;
+    }
+
+    public int getStartHeightPart() {
+        return startHeightPart;
+    }
+
+    public int getStartWidthPart() {
+        return startWidthPart;
+    }
+}
