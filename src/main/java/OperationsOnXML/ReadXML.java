e2a2b1b37d003dd118bd86bb0d14f0e99ace8c0f
Matti <mateusz.surma.f@gmail.com>
2021-10-09 19:24:44 +0200

    add change order parameters

--- src/main/java/OperationsOnXML/ReadXML.java
+++ src/main/java/OperationsOnXML/ReadXML.java
@@ -1,20 +1,17 @@
 package OperationsOnXML;
 
-import javax.xml.parsers.DocumentBuilderFactory;
+import hue.Client;
+import lombok.SneakyThrows;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
-import hue.Client;
-import lombok.SneakyThrows;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Element;
-
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.Scanner;
